97ca24bb510bc4780ef4f1405e5dae683ceeab3b
wx <2523058044@qq.com>
2024-11-22 13:31:08 +0800

    first commit (demo)

---
+++ hardhat.config.ts
@@ -0,0 +1,47 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "solidity-coverage";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { PRIVATE_KEY } = process.env;
+// personal experience subjective healthcare smartwatch preventive good for environment(walk more) benefit society
+// toC 
+// value convice people
+// have to has an economic model
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10000,
+      },
+    },
+  },
+  typechain: {
+    outDir: "typechain-types",
+    target: "ethers-v6",
+  },
+  networks: {
+    gemini: {
+      url: "https://rpc5.gemini.axiomesh.io",
+      accounts: PRIVATE_KEY ? [process.env.PRIVATE_KEY!] : [],
+    },
+    local: {
+      url: "http://localhost:8545",
+      accounts: [
+        "",
+      ],
+    },
+    hardhat: {
+      accounts: {
+        mnemonic: process.env.SEED_PHRASE,
+      },
+      chainId: 1337,
+    },
+  },
+};
+
+export default config;
